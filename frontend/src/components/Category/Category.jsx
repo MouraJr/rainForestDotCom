995689ab15f32e57de97f848e7aadee4fe38f29a
Geraldo Moura <geraldo.moura@ymail.com>
2021-11-12 13:20:19 -0600

    stopping point 🛑

--- frontend/src/components/Category/Category.jsx
+++ frontend/src/components/Category/Category.jsx
@@ -11,7 +11,7 @@ const Category = ({ data }) => {
     return (
         <div className='row justify-content-center'>
             {data ? data.map((item) =>
-                <Card border="primary" className="m-4 m-sm-1 p-3 p-sm-1 col-sm-4 col-md-2" key={item.id}>
+                <Card border="primary" className="m-4 p-3 p-sm-1 col-8 col-sm-6 col-md-3" key={item.id}>
                     <Card.Img variant="top" src={item.image_url} />
                     <Card.Body>
                         <Card.Title>{item.name}</Card.Title>
