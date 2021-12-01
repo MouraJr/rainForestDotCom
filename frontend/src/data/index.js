e7358287fa616b51d7d9025f807f6a099da86d07
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-01 14:48:46 -0600

    added https for fetchdata

--- frontend/src/data/index.js
+++ frontend/src/data/index.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 require('dotenv').config({ path: '../../../.env' });
 
-// const url = 'http://3.140.201.84:5000/';
-const url = process.env.URL || 'http://3.138.246.150:5000/';
+// const url = 'https://3.140.201.84:5000/';
+const url = process.env.URL || 'https://3.138.246.150:5000/';
 
 export const fetchData = async (category) => {
     let dataUrl = `${url}`
