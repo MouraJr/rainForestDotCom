bba2bb59f72f84f55910c8180af7552247a48eaa
Geraldo Moura <geraldo.moura@ymail.com>
2021-12-03 14:37:44 -0600

    Test Deployment

--- frontend/src/components/FrontPage/FrontPage.jsx
+++ frontend/src/components/FrontPage/FrontPage.jsx
@@ -24,7 +24,7 @@ export default class FrontPage extends Component {
             <div className="d-flex align-items-center justify-content-center h-100 w-100">
                 <div className="card card-primary p-4">
                     <div id={'log-in-credentials'} className="row">
-                        <p>Testing deployment!</p>
+                        <p>Testing deployment!!!!</p>
                         <input className="my-1" id={'username'} value={this.state.username} onChange={e => this.setState({ username: e.currentTarget.value })} name={'username'} placeholder={'Username'} />
                         {/* <input className="my-1" id={'password'} value={this.state.password} onChange={event => this.updatePasswordValue(event)} name={'password'} placeholder={'Password'} /> */}
                         <button className="my-1 btn btn-success" id={'sign-in'} onClick={this.onClick}>Sign In</button>
